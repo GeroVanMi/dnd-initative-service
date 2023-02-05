8fbdfc90fe6f5eadff88c9d3d62c8a657f0044ee
Gerome Meyer <gerome.meyer@icloud.com>
2023-02-05 07:14:50 +0100

    Added createRelease script

---
+++ createRelease.sh
@@ -0,0 +1,8 @@
+if [ -z "$1" ]
+  then
+    echo "Supply a version number. E.g. bash createRelease.sh 0.0.1"
+    exit 1
+fi
+
+sudo docker build -t gerovanmi/dnd-initiative-service -t gerovanmi/dnd-initiative-service:$1 .
+sudo docker push -t gerovanmi/dnd-initiative-service -t gerovanmi/dnd-initiative-service:$1
